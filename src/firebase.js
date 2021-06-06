305ba1cc6f66e3b58334d47e4cfe88da4337b853
Isiah Jones <jones.isiah.blhs@gmail.com>
2021-06-06 15:24:56 -0400

    update

--- src/firebase.js
+++ src/firebase.js
@@ -11,7 +11,7 @@ import "firebase/storage"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
-  authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
+  authDomain: "disneyplus-clone-a9549.firebaseapp.com",
   projectId: "disneyplus-clone-a33d5",
   storageBucket: "disneyplus-clone-a33d5.appspot.com",
   messagingSenderId: "37918794208",
