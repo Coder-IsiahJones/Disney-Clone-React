3209d3ea01f7c5109ab229fd9c026af7932ed015
Isiah Jones <jones.isiah.blhs@gmail.com>
2021-06-06 12:38:13 -0400

    Created Image Slider with style-components.

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,22 +1,27 @@
-import React from "react"
 import styled from "styled-components"
-import Slider from "react-slick"
-
-// Import css files (react-slick)
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
+import Slider from "react-slick"
 
 const Carousel = styled(Slider)`
   margin-top: 20px;
 
-  .slick-list {
-    overflow: visible;
+  & > button {
+    opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
+
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease 0s;
+    }
   }
 
   ul li button {
     &:before {
       font-size: 10px;
-      color: rgba(150, 158, 171);
+      color: rgb(150, 158, 171);
     }
   }
 
@@ -24,30 +29,47 @@ const Carousel = styled(Slider)`
     color: white;
   }
 
-  button {
-    z-index: 1;
+  .slick-list {
+    overflow: initial;
+  }
+
+  .slick-prev {
+    left: -75px;
+  }
+
+  .slick-next {
+    right: -75px;
   }
 `
 
 const Wrap = styled.div`
+  border-radius: 4px;
   cursor: pointer;
+  position: relative;
 
-  img {
-    width: 100%;
-    height: 100%;
+  a {
     border-radius: 4px;
-    box-shadow: rgba(0 0 0 / 69%) 0px 26px 30px -10px,
-      rgba(0 0 0 / 73%) 0px 16px 10px -10px;
-    border: 4px solid transparent;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    cursor: pointer;
+    display: block;
+    position: relative;
+    padding: 4px;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
 
     &:hover {
+      padding: 0;
       border: 4px solid rgba(249, 249, 249, 0.8);
       transition-duration: 300ms;
     }
   }
 `
 
-const ImgSlider = () => {
+const ImgSlider = (props) => {
   let settings = {
     dots: true,
     infinite: true,
@@ -56,14 +78,30 @@ const ImgSlider = () => {
     slidesToScroll: 1,
     autoplay: true,
   }
-
   return (
     <Carousel {...settings}>
       <Wrap>
-        <img src='/images/slider-badging.jpg' alt='' />
+        <a>
+          <img src='/images/slider-badging.jpg' alt='' />
+        </a>
       </Wrap>
+
       <Wrap>
-        <img src='/images/slider-badag.jpg' alt='' />
+        <a>
+          <img src='/images/slider-scale.jpg' alt='' />
+        </a>
+      </Wrap>
+
+      <Wrap>
+        <a>
+          <img src='/images/slider-badag.jpg' alt='' />
+        </a>
+      </Wrap>
+
+      <Wrap>
+        <a>
+          <img src='/images/slider-scales.jpg' alt='' />
+        </a>
       </Wrap>
     </Carousel>
   )
